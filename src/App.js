00128f1c94ebed7066541d000241c58198c9e228
Sluis <paul.sluis@dxc.com>
2021-05-26 23:44:08 +1000

    fixed the frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
+  'use strict';
+
+  const JSON_STRINGIFY_INDENT = 2;
+  const e = React.createElement;
+
+  class EventCaller extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { eventBody: '' };
+  }
+
+  componentDidMount() {
+    this.GetEvent();
+  }
+
+  GetEvent() {
+      fetch(this.props.endpoint)
+      .then(response => {
+        return response.json();
+      })
+      .then(responseBody => {
+          this.setState({ eventBody: JSON.stringify(responseBody, null, JSON_STRINGIFY_INDENT)});
+      });
+  }
+
+  render() {
+    let eventBody = this.state.eventBody;
+    return (
+      <div id="event-body-text">
+        <h2>Event Body Will Be Output Below</h2>
+        <pre>
+          { eventBody }
+        </pre>
+      </div>
+    );
+  }
+}
+
+var endpoint_url = document.getElementById('endpoint_url').innerHTML.trim();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My content here in App.js
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          (I need to Learn React?!)
-        </a>
-      </header>
+    <div className="p#endpoint_url">
+
+     <script src="https://unpkg.com/react@^16/umd/react.production.min.js" crossorigin></script>
+     <script src="https://unpkg.com/react-dom@^16/umd/react-dom.production.min.js" crossorigin></script>
+     <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+     <EventCaller endpoint={endpoint_url}/>
+    
     </div>
   );
 }
