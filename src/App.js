1b0d8e54b69fe169b1d762f3ff585351705a2b89
Sluis <paul.sluis@dxc.com>
2021-05-26 22:20:51 +1000

    removed frontend libraries, updated app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ARGH!!!
+          My content here in App.js
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          (I need to Learn React?!)
         </a>
       </header>
     </div>
